326ba4be95ff0605023d037b95b839c8f30a85ef
Jackson Seely <jseely@umass.edu>
2026-09-18 22:34:17 -0400

    add About page

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ import pagesRouter from './routes/pages.js';
 import apiRouter from './routes/api.js';
 
 const app = express();
+app.set("view engine", "ejs");
 const PORT = process.env.PORT || 3000;
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
 
 app.use('/', pagesRouter);
 app.use('/api', apiRouter);
 
+
+
 app.use((req, res) => {
   res.status(404).send('Page not found.');
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
